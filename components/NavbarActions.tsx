e8734991529453b728950cf10b1a2d8a34147ec5
kanishkumar11 <kanishkumar11@outlook.in>
2023-12-19 00:05:56 +0530

    first commit

--- components/NavbarActions.tsx
+++ components/NavbarActions.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Button from "./ui/Button";
+import Button from "./ui/button";
 import { ShoppingBag } from "lucide-react";
 
 const NavbarActions = () => {
